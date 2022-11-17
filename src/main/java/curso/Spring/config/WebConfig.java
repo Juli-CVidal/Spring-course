3a1158c0ac005724f94413ad84b08c0d3ed11713
Juli-CVidal <juli.vidal.019@gmail.com>
2022-11-17 10:32:45 -0300

    Added part of Spring Security

--- src/main/java/curso/Spring/config/WebConfig.java
+++ src/main/java/curso/Spring/config/WebConfig.java
@@ -1,5 +1,4 @@
-package curso.Spring;
-
+package curso.Spring.config;
 
 import java.util.Locale;
 
@@ -7,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -21,7 +21,6 @@ public class WebConfig implements WebMvcConfigurer{
 		return resolver;
 	}
 	
-	
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
 		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
@@ -32,5 +31,11 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/login");
 	}
 }
